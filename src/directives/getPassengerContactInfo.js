e2badef799933f307e2d08d4b611b93a474d4457
LitvinenkoEvgeny <yourbisnesspartner@gmail.com>
2018-12-21 14:57:20 +0400

    42138 Дополнительные поля в pop-up окне. & build

--- src/directives/getPassengerContactInfo.js
+++ src/directives/getPassengerContactInfo.js
@@ -12,20 +12,24 @@ app.directive('getPassengersContactInfo', function () {
 app.controller('getPassengersContactInfoController', ['$scope', '$rootScope', getPassengersContactInfoController]);
 
 function getPassengersContactInfoController($scope, $rootScope) {
-    var vm = $scope;
-
     $scope.$watch(angular.bind(this, function () {
         if (
             $rootScope.$$childHead &&
             $rootScope.$$childHead.vm &&
             $rootScope.$$childHead.vm.customerForm &&
             $rootScope.$$childHead.vm.customerForm.email
-        ){
-            return $rootScope.$$childHead.vm.customerForm.email.$modelValue
+        ) {
+            return $rootScope.$$childHead.vm.customerForm
         }
-    }), function (newCustomerEmail) {
-        if (newCustomerEmail) {
-            $scope.vm.customerEmail = newCustomerEmail;
-        }
-    });
+    }), function () {
+
+        $scope.vm.customer = {};
+
+        $scope.vm.customer.phone = $rootScope.$$childHead.vm.customerForm.phone.$modelValue;
+        $scope.vm.customer.email = $rootScope.$$childHead.vm.customerForm.email.$modelValue;
+        $scope.vm.customer.lastName = $rootScope.$$childHead.vm.customerForm.lastName.$modelValue;
+        $scope.vm.customer.firstName = $rootScope.$$childHead.vm.customerForm.firstName.$modelValue;
+
+
+    }, true);
 }
