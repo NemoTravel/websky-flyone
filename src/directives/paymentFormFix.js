199969f833d654c2076826be4733a5a0feff4804
LitvinenkoEvgeny <yourbisnesspartner@gmail.com>
2019-01-11 14:00:42 +0400

    42415 Ошибка при покупке доп. багажа. (web check-in) remove default checked property from input

--- src/directives/paymentFormFix.js
+++ src/directives/paymentFormFix.js
@@ -10,18 +10,43 @@ app.directive('paymentFormFix', function () {
         bindToController: true,
         controller: 'paymentFormFixController',
         controllerAs: 'vm',
-    }
+
+        // поскольку значения по умолчанию теперь пустые, нужно снимать
+        // атрибут checked с инпута, сейчас input[checked=true] т.к в верстке payment-type-select
+        // присутствует код
+        // !vm.selectedPaymentType || vm.selectedPaymentType === 'plastic_card'
+        // поскольку в контроллере мы устанавливаем vm.paymentForm = vm.paymentType = null
+        // условие !vm.selectedPaymentType срабатывает и инпут выставляется как checked
+        link: function (scope, el) {
+            var elementPresentOnPageInterval = setInterval(function () {
+                var input = el.find('input:checked');
+
+                if (input.length) {
+                    input.prop('checked', false);
+                    clearInterval(elementPresentOnPageInterval);
+                }
+            }, 200);
+        }
+}
 });
 
 app.controller('paymentFormFixController', ['$scope', paymentFormFixController]);
 
 function paymentFormFixController($scope) {
     var vm = this;
-    console.log($scope, vm);
 
     $scope.$watch(angular.bind(this, function () {
         return vm.paymentType;
     }), function (newPaymentType) {
+
+        // удалить дефолтное значение из выбранного способа оплаты.
+        if (_.isEqual(vm.paymentForm, {code: 'QW', name: 'Qiwi'})) {
+            vm.paymentForm = vm.paymentType = null;
+        }
+
+        // #42415
+        // Если где-то изменяется значение vm.paymentType на plastic_card
+        // Обновить vm.paymentForm
         if (newPaymentType === 'plastic_card') {
             vm.paymentForm = 'plastic_card';
         }
